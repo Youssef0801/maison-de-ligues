70a3af6b000fb2962a9685eda7734b49e70ea936
Youssef0801 <assehnoune.youssef@gmail.com>
2025-04-03 17:30:45 +0200

    Ajout de l'inclusion progressive PHP dans index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="fr">
+<?php include_once 'include/head.inc.php'; ?>
+<body>
+
+<!-- Inclusion des contrôleurs PHP -->
+<?php include_once 'include/controllers.inc.php'; ?>
+
+<!-- Exemple d'inclusion de liste -->
+<?php include_once 'src/liste.inc.php'; ?>
+
+</body>
+</html>
